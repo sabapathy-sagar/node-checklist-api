356e3b53907981caa51ef47f8c08d9d2a25218c0
sagar rathna sabapathy <sagar.rathnasabapathy@diconium.com>
2018-07-29 14:00:38 +0200

    GET checklist based on id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 const mongoose = require('./db/mongoose');
 const {Checklist} = require('./models/checklist');
 const {User} = require('./models/user');
@@ -43,6 +44,33 @@ app.get('/checklists', (req,res) => {
             //on fetch failed send the error message with a status of 400 in the GET response
             res.status(400).send(e);
     })
+});
+
+//GET method endpoint to fetch a specific chdecklist based on the id provided
+app.get('/checklists/:id', (req,res) => {
+    const id = req.params.id;
+    //validate the id provided, if invalid send 404 Bad request
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+        return;
+    }
+
+    //fetch the checklist based on the id from the db
+    Checklist.findById(id)
+        .then(
+            (checklist) => {
+                //if no checklist exists for the id, send 404 with empty body
+                if(!checklist){
+                   return res.status(404).send();
+                }
+                //if checklist exists, send the fetched checklist back
+                res.send({checklist});
+        })
+        .catch((() => {
+            //if error while fetching the checklist, send status 400 with empty body
+            res.status(400).send();
+        }))
+
 });
 
 //listen on port 3000
