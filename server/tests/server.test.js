a5ea8a47a50d7184692e694579818553c72e1a59
sagar rathna sabapathy <sagar.rathnasabapathy@diconium.com>
2018-08-05 10:22:30 +0200

    unit tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -207,3 +207,32 @@ describe('POST users/login', () => {
 
     })
 })
+
+describe('DELETE users/me/token', () => {
+    it('should delete the token from the tokens array', (done) => {
+
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e))
+            })
+    })
+
+    it('should return 401 error if token not found', (done) => {
+
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', 'blabalabla')
+            .expect(401)
+            .end(done)
+    })
+})
